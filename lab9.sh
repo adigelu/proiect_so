3a615eb61971e6900f2670615adda9c5d1874fff
adigelu <150262426+adigelu@users.noreply.github.com>
2023-11-29 17:02:38 +0200

    Add files via upload

--- lab9.sh
+++ lab9.sh
@@ -5,8 +5,11 @@ functie()
     caracter=$1;
     while  read -r line;
     do
-	
-	((count++))
+    echo "$line" | grep -E "^[A-Z][A-Za-z0-9 ,.!?]*[.!?]$" | grep -v ",si" | grep -E -c $caracter
+    if [ $? -eq 0 ] 
+    then
+        ((count++))
+    fi
     done
     echo $count
 }
